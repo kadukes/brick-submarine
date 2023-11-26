7d94eeb902335230ab679ff4f4e06f869f15f8ed
kadukes <k.dede@localhost.de>
2023-11-26 20:25:59 +0100

    add import statements and log config to main

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
-from sensor import pressure
+import sys
+from time import sleep
+import logging
 
+import buoyancy
+from sensor import tacho
+from sensor import pressure
+from sensor import sonar
+
+import navigation
+from sensor import gyro
+
+import power
+from sensor import adc
+
+
+FORMAT = '[%(levelname)s] %(asctime)s - %(message)s'
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG, format=FORMAT, stream=sys.stdout) #, filename='ship.log')
